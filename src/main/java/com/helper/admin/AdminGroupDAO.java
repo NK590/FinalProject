49eedbe36e85f028132b2735819af1f2be8aa44c
shinyoung hong <sy2667@naver.com>
2022-07-27 13:38:18 +0900

    AdminGroupDAO name change

--- src/main/java/com/helper/admin/AdminGroupDAO.java
+++ src/main/java/com/helper/admin/AdminGroupDAO.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class GroupDAO {
+public class AdminGroupDAO {
 	
 	@Autowired
 	private SqlSession session;
--- src/main/java/com/helper/admin/AdminService.java
+++ src/main/java/com/helper/admin/AdminService.java
@@ -12,7 +12,7 @@ public class AdminService {
 	@Autowired
 	private ReportDAO reportDAO;
 	@Autowired
-	private GroupDAO groupDAO;
+	private AdminGroupDAO groupDAO;
 	@Autowired
 	private RecordDAO recordDAO;
 	
