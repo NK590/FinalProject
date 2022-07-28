3763226cfedd525487a89bd90d981a1a6702b2d0
minhusong <dnltnzla1@naver.com>
2022-07-28 14:33:37 +0900

    calendar created

--- src/main/java/com/helper/admin/AdminController.java
+++ src/main/java/com/helper/admin/AdminController.java
@@ -15,45 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/admin")
 public class AdminController {
 	
-	@Autowired
-	private AdminService service;
-	
-	@RequestMapping(value = "/toBlacklist")
-	public String toBlackList(int curPage, Model model) throws Exception {
-		List<ReportDTO> reportList = service.reportList(curPage*10-9, curPage*10);
-		
-		// 페이징 Map
-		Map<String, Object> map = service.getBleckPageNavi(curPage);
-		model.addAttribute("reportList", reportList);
-		model.addAttribute("naviMap", map);
-		return "admin/blacklist";
-	}
-	
-	@RequestMapping(value = "/addBleck")
-	public String addBleck(int mem_seq) throws Exception {
-		service.addBleck(mem_seq);
-		return "redirect:/admin/toBlacklist?curPage=1";
-	}
-	
-	@RequestMapping(value = "/searchMember")
-	@ResponseBody
-	public Map<String, Object> searchMember(String mem_id, int curPage) throws Exception {
-		List<ReportDTO> dto = service.searchMember(mem_id, curPage*10-9, curPage*10);
-		for(ReportDTO list : dto) {
-			System.out.println(list);
-		}
-		System.out.println("===");
-		Map<String, Object> map = new HashedMap<String, Object>();
-		map.put("list", dto);
-		map.put("naviMap", service.getSearchPageNavi(mem_id, curPage));
-		map.put("mem_id", mem_id);
-		return map;
-	}
-	
-	@RequestMapping(value = "/grouplist")
-	public String grouplist() throws Exception {
-		return "admin/grouplist";
-	}
 	
 	
 	
--- src/main/java/com/helper/planner/PlannerService.java
+++ src/main/java/com/helper/planner/PlannerService.java
@@ -1,5 +1,9 @@
 package com.helper.planner;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +11,51 @@ import org.springframework.stereotype.Service;
 public class PlannerService {
 	@Autowired
 	private PlannerDAO dao;
+
+	public int planInsert(PlannerDTO dto) throws Exception { // 일정 추가
+		int plan_seq = dao.selectSeq();// 플래너의 시퀀스
+		dto.setPlan_seq(plan_seq);
+		System.out.println("after : "+dto.toString());
+		return dao.planInsert(dto);
+	}
+	public int delete(Map<String,Object> jsonData)throws Exception{ // 일정 삭제
+//			System.out.println(jsonData);			
+//			String plan_title = (String)jsonData.get("plan_title"); 
+//			String plan_start = (String)jsonData.get("plan_start");
+//			String plan_end = (String)jsonData.get("plan_end");
+			int plan_seq = Integer.parseInt((String)jsonData.get("plan_seq"));
+//			Map<String,Object> map = new HashMap<>();
+//			map.put("plan_title", plan_title);
+//			map.put("plan_start", plan_start);
+//			map.put("plan_end", plan_end);
+//			map.put("plan_seq", plan_seq);			
+			System.out.println(plan_seq);
+			
+			
+		return dao.delete(plan_seq);
+	}
+	public int update(Map<String,Object>jsonData)throws Exception{ // 일정 수정
+		String plan_start = (String)jsonData.get("plan_start");
+		String plan_end = (String)jsonData.get("plan_end");
+		int plan_seq = Integer.parseInt((String)jsonData.get("plan_seq"));
+		Map<String,Object> map = new HashMap<>();
+		map.put("plan_start", plan_start);
+		map.put("plan_end", plan_end);
+		map.put("plan_seq", plan_seq);
+		System.out.println("updateMap : "+map);		
+		return dao.update(map);
+	}
+	
+	
+	public List<PlannerDTO> selectAll()throws Exception{ // 모든 일정 출력	
+		
+		return dao.selectAll();
+	}
 	
 }
+
+
+
+
+
+
